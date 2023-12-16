6e7d37d3837c239be5190b7053aef2c8a320df55
Adrian <alextravc.342@gmail.com>
2023-12-16 18:27:49 -0500

    [ADD]: assets + initial home design of 01-main-demo.html of hiStudy template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+////* init home js
+declare function homeInit([]): any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online-courses';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      homeInit($);
+    }, 50);
+  }
 }
