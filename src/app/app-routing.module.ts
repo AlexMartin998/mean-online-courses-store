f33609a35f3c54bfa3c2671be7d10b8dc20f8d97
Adrian <alextravc.342@gmail.com>
2023-12-18 17:00:14 -0500

    [ADD]: authGuard applied in /auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { authGuard } from './auth/guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -10,6 +12,7 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [authGuard],
   },
 
   { path: '**', redirectTo: '/' },
