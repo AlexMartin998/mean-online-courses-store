fa77049583d7b3efea509df55757c9da571d9ce0
Adrian <alextravc.342@gmail.com>
2023-12-18 11:57:55 -0500

    [ADD]: AuthLayoutComponent

---
+++ src/app/auth/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-auth-layout',
+  templateUrl: './auth-layout.component.html',
+  styles: [],
+})
+export class AuthLayoutComponent {}
