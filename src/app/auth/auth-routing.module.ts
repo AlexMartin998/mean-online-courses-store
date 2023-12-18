3dc1fd9f51ba73e89ae106a4ceb91f5d9c3e5e46
Adrian <alextravc.342@gmail.com>
2023-12-18 11:58:44 -0500

    [ADD]: AuthLayoutComponent and AuthLayoutComponent included in AuthRoutingModule

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+import { LoginAndRegisterPageComponent } from './pages/login-and-register-page/login-and-register-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [{ path: 'login', component: LoginAndRegisterPageComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
+
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 import { LoginAndRegisterPageComponent } from './pages/login-and-register-page/login-and-register-page.component';
 
 @NgModule({
-  declarations: [LoginAndRegisterPageComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [LoginAndRegisterPageComponent, AuthLayoutComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
