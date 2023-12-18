ca863f2ddea9091990c8f7e4b2944f80024fb447
Adrian <alextravc.342@gmail.com>
2023-12-18 11:22:27 -0500

    [ADD]: HomePageComponent

---
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-home-page',
+  templateUrl: './home-page.component.html',
+  styles: [
+  ]
+})
+export class HomePageComponent {
+
+}
