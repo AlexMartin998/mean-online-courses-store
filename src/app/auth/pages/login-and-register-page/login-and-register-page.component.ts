5d6a4eda0686722cc3925c3deb95171d3332b188
Adrian <alextravc.342@gmail.com>
2023-12-18 14:58:22 -0500

    [ADD]: loginForm linked with login html form

--- src/app/auth/pages/login-and-register-page/login-and-register-page.component.ts
+++ src/app/auth/pages/login-and-register-page/login-and-register-page.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-and-register-page',
   templateUrl: './login-and-register-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginAndRegisterPageComponent {
+  public loginForm: FormGroup = this.fb.group({
+    email: ['alex1@test.com', [Validators.required, Validators.email]],
+    password: ['123123', [Validators.required, Validators.minLength(6)]],
+  });
 
+  constructor(
+    private readonly authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  onLogin() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.showWelcomeMessageAndRedirect();
+      },
+      error: (errorMessage) => {
+        alert(errorMessage);
+        // this.showSnackbar(errorMessage);
+      },
+    });
+  }
+
+  private showWelcomeMessageAndRedirect() {
+    const authUser = this.authService.currentUser;
+
+    // // with signals this navigation is not necessary
+    // redirect based on user role
+    // if (authUser?.role.name === 'ADMIN')
+    //   return this.router.navigateByUrl('/admin/books');
+
+    return this.router.navigateByUrl('/');
+  }
 }
