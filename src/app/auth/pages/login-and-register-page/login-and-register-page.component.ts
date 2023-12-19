4e743a2642c71be3364095a188089af0f44e5a94
Adrian <alextravc.342@gmail.com>
2023-12-18 23:47:38 -0500

    [ADD]: connect registerForm with html form and set error messages for each field

--- src/app/auth/pages/login-and-register-page/login-and-register-page.component.ts
+++ src/app/auth/pages/login-and-register-page/login-and-register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -11,18 +12,49 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginAndRegisterPageComponent {
   public loginForm: FormGroup = this.fb.group({
-    email: ['alex1@test.com', [Validators.required, Validators.email]],
+    // defaultValue, [sync validations], [async validations]
+    email: [
+      'alex1@test.com',
+      [Validators.required, Validators.pattern(ValidatorsService.emailPattern)],
+    ],
     password: ['123123', [Validators.required, Validators.minLength(6)]],
   });
 
+  public registerForm: FormGroup = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      surname: ['', [Validators.required]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(ValidatorsService.emailPattern),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    },
+
+    // Tengo acceso a Todos los Controls del Form
+    {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo(
+          'password',
+          'confirmPassword'
+        ),
+      ],
+    }
+  );
+
   constructor(
     private readonly authService: AuthService,
+    private readonly validatorsService: ValidatorsService,
     private fb: FormBuilder,
     private router: Router
   ) {}
 
   onLogin() {
-    if (this.loginForm.invalid) return;
+    if (this.loginForm.invalid) return this.loginForm.markAllAsTouched();
 
     this.authService.login(this.loginForm.value).subscribe({
       next: () => {
@@ -33,6 +65,15 @@ export class LoginAndRegisterPageComponent {
         // this.showSnackbar(errorMessage);
       },
     });
+  }
+
+  onRegister() {
+    if (this.registerForm.invalid) return this.registerForm.markAllAsTouched();
+    console.log(this.registerForm.value);
+  }
+
+  isInvalidField(field: string) {
+    return this.validatorsService.isInvalidField(this.registerForm, field);
   }
 
   private showWelcomeMessageAndRedirect() {
